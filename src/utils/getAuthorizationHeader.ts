ef1bc95b415a230a663dfdf74d146f6bd6a8839e
Duke <hibary.taiwan@gmail.com>
2023-12-20 21:55:18 +0800

    feat: api 攔截器 and 資料讀取

---
+++ src/utils/getAuthorizationHeader.ts
@@ -0,0 +1,21 @@
+// import jsSHA from 'jssha'
+import { CookieDao } from './dao'
+
+
+export function getAuthorizationHeader(): any {
+    const authToken = new CookieDao().read('Authorization');
+
+    const GMTString = new Date().toUTCString()
+    // const shaObj = new jsSHA('SHA-1', 'TEXT')
+    // shaObj.setHMACKey("appKey", 'TEXT')
+    // shaObj.update('x-date: ' + GMTString)
+    //const HMAC = shaObj.getHMAC('B64')
+    //const Authorization = `hmac  algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`
+
+    return {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'X-Date': GMTString,
+        'Authorization': authToken,
+    }
+}
